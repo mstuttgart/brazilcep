ee2bcc367735663890895e446c48e43ef95bf240
mstuttgart <michellstut@gmail.com>
2015-09-08 22:27:03 -0300

    Adicionado novos testes

--- correios_cep.py
+++ correios_cep.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 # #############################################################################
 #   Copyright (C) 2015  Michell Stuttgart
+#
 #   This program is free software: you can redistribute it and/or modify it
 #   under the terms of the GNU General Public License as published by the Free
 #   Software Foundation, either version 3 of the License, or (at your option)
 #   any later version.
+#
 #   This program is distributed in the hope that it will be useful, but WITHOUT
 #   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 #   more details.
+#
 #   You should have received a copy of the GNU General Public License along
 #   with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -51,7 +54,7 @@ class CorreiosCEP(object):
                 cep.complemento2.encode('utf8')) if cep.complemento2 else '',
             'rua': str(cep.end.encode('utf8')) if cep.end else '',
             'id': cep.id,
-            'UF': str('res.uf'),
+            'UF': str(cep.uf),
         }
 
         return cep_dict
