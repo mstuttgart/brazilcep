2e368363e5cebe0d09f145a2e9908fbe45d742d6
Michell Stuttgart <michellstut@gmail.com>
2017-04-22 17:05:50 -0300

    [IMP] XML de teste gerado utilizando templates

--- test/test_correios.py
+++ test/test_correios.py
@@ -3,6 +3,7 @@
 import requests
 from unittest import mock
 from unittest import TestCase
+from jinja2 import Environment, FileSystemLoader
 
 from pycep_correios.correios import Correios
 from pycep_correios.correios_exceptions import \
@@ -18,45 +19,37 @@ from pycep_correios.correios_exceptions import \
 class TestCorreios(TestCase):
 
     def setUp(self):
-        self.response_xml = '''<S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\">
-                                    <S:Body>
-                                    <ns2:consultaCEPResponse
-                                    xmlns:ns2=\"http://cliente.bean.master.sigep.bsb.correios.com.br/\">
-                                        <return>
-                                            <bairro>Asa Norte</bairro>
-                                            <cep>70002900</cep>
-                                            <cidade>Brasília</cidade>
-                                            <complemento/>
-                                            <complemento2/>
-                                            <end>SBN Quadra 1 Bloco A</end>
-                                            <id>0</id>
-                                            <uf>DF</uf>
-                                        </return>
-                                    </ns2:consultaCEPResponse>
-                                    </S:Body>
-                                    </S:Envelope>'''.replace('\n', '')
 
-        self.response_xml_error = '''<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-                <soap:Body><soap:Fault>
-                <faultcode>soap:Server</faultcode>
-                <faultstring>BUSCA DEFINIDA COMO EXATA, 0 CEP DEVE TER 8 DIGITOS</faultstring>
-                <detail><ns2:SigepClienteException xmlns:ns2="http://cliente.bean.master.sigep.bsb.correios.com.br/">
-                BUSCA DEFINIDA COMO EXATA, 0 CEP DEVE TER 8 DIGITOS
-                </ns2:SigepClienteException></detail></soap:Fault>
-                </soap:Body></soap:Envelope>'''.replace('\n', '')
-
-        self.expected_address = {
+        address = {
             'bairro': 'Asa Norte',
+            'cep': '70002900',
             'cidade': 'Brasília',
-            'complemento': '',
-            'outro': '',
-            'rua': 'SBN Quadra 1 Bloco A',
+            'end': 'SBN Quadra 1 Bloco A',
+            'id': '0',
             'uf': 'DF',
         }
 
+        self.expected_address = {
+            'bairro': address['bairro'],
+            'cidade': address['cidade'],
+            'complemento': '',
+            'outro': '',
+            'rua': address['end'],
+            'uf': address['uf'],
+        }
+
+        self.env = Environment(loader=FileSystemLoader('test/templates'))
+
+        template = self.env.get_template('response.xml')
+        xml = template.render(**address)
+        self.response_xml = (xml.replace('\n', '')).replace('\t', '')
+
+        template = self.env.get_template('response_error.xml')
+        xml = template.render()
+        self.response_xml_error = (xml.replace('\n', '')).replace('\t', '')
+
     @mock.patch('pycep_correios.correios.requests.post')
     def test_get_cep(self, mock_api_call):
-
         mock_api_call.return_value = mock.MagicMock(status_code=200,
                                                     ok=True,
                                                     text=self.response_xml)
@@ -91,31 +84,18 @@ class TestCorreios(TestCase):
                           Correios._format_cep, 37503003)
 
     def test__mount_request(self):
-
-        HEADER = '<soap:Envelope ' \
-             'xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" ' \
-             'xmlns:cli=\"http://cliente.bean.master.sigep.bsb.correios.com' \
-             '.br/\"><soap:Header/><soap:Body>'
-
-        FOOTER = '</soap:Body></soap:Envelope>'
-
-        cep='37503005'
-
-        xml = HEADER
-        xml += '<cli:consultaCEP>'
-        xml += '<cep>%s</cep>' % cep
-        xml += '</cli:consultaCEP>'
-        xml += FOOTER
+        cep = '37503005'
+        template = self.env.get_template('consultacep.xml')
+        xml = template.render(cep=cep)
+        xml = (xml.replace('\n', '')).replace('\t', '')
 
         self.assertEqual(xml, Correios._mount_request(cep=cep))
 
     def test__parse_response(self):
-
         response = Correios._parse_response(self.response_xml)
         self.assertDictEqual(response, self.expected_address)
 
     def test__parse_error(self):
-
         fault = Correios._parse_error(self.response_xml_error)
-        self.assertEqual(fault, 'BUSCA DEFINIDA COMO EXATA, '
-                                '0 CEP DEVE TER 8 DIGITOS')
+        self.assertEqual(fault.strip(), 'BUSCA DEFINIDA COMO EXATA, '
+                                        '0 CEP DEVE TER 8 DIGITOS')
