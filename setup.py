939e0867ae086c3e05785a85ad1d5716b0115d3f
Michell Stuttgart <michellstut@gmail.com>
2017-04-29 16:53:55 -0300

    [REF] Ajuestes formatacao do codigo no setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ setup(
     packages=find_packages(exclude=['*test*']),
     zip_safe=False,
     url='https://github.com/mstuttgart/pycep-correios',
-    license='MIT',
+    license='MIT License',
     author='Michell Stuttgart',
     author_email='michellstut@gmail.com',
-    description=u'Método para busca de dados de CEP no webservice dos '
-                'Correios',
+    description='Método para busca de dados de CEP no webservice dos Correios',
     long_description=open('README.md', 'r').read(),
     package_data={
-            'pycep-correios': ['templates/*xml']
+        'pycep-correios': ['templates/*xml']
     },
+    include_package_data=True,
     install_requires=[
         'requests >= 2.10.0',
         'Jinja2 >= 2.8',
@@ -34,6 +34,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        
     ],
 )
