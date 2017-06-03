ce6d2426163e9eaaa19779b89b5c7fad1667b0d2
Michell Stuttgart <michellstut@gmail.com>
2017-06-03 18:07:11 -0300

    [IMP] Update setup files

--- setup.py
+++ setup.py
@@ -1,32 +1,45 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-from pycep_correios import __version__
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'requests >= 2.10.0',
+    'Jinja2 >= 2.8',
+]
+
+test_requirements = [
+    'coverage',
+    'coveralls',
+]
 
 setup(
-    name='pycep-correios',
-    version=__version__,
-    keywords='correios development cep',
-    packages=find_packages(exclude=['*test*']),
-    zip_safe=False,
-    url='https://github.com/mstuttgart/pycep-correios',
-    download_url='https://pypi.python.org/pypi/pycep-correios',
-    license='MIT License',
+    name='pycep_correios',
+    version='1.1.7',
+    description='Método para busca de dados de CEP no webservice dos Correios',
+    long_description=readme + '\n\n' + history,
     author='Michell Stuttgart',
     author_email='michellstut@gmail.com',
     maintainer='Michell Stuttgart',
     maintainer_email='michellstut@gmail.com',
-    description='Método para busca de dados de CEP no webservice dos Correios',
-    long_description=open('README.rst', 'r').read(),
-    install_requires=[
-        'requests >= 2.10.0',
-        'Jinja2 >= 2.8',
+    url='https://github.com/mstuttgart/pycep_correios',
+    download_url='https://pypi.python.org/pypi/pycep-correios',
+    packages=[
+        'pycep_correios',
     ],
-    test_suite='test',
-    tests_require=[
-        'coverage',
-        'coveralls',
-    ],
+    package_dir={'pycep_correios':
+                 'pycep_correios'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='correios development cep',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -39,4 +52,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
     ],
+    test_suite='tests',
+    tests_require=test_requirements,
 )
