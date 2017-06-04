8849dee4107c02c64b59c0e3940877ea144cd29d
Michell Stuttgart <michellstut@gmail.com>
2017-06-04 19:04:00 -0300

    [NEW] Add python 3.6 in setup.py

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
     ],
     test_suite='tests',
