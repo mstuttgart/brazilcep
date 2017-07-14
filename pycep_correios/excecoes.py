c9792574b89d6580d82fc3eed612a2a703d61b97
Michell Stuttgart <michellstut@gmail.com>
2017-07-13 21:57:29 -0300

    [NEW] Adicionado nvos tipos de excecao

--- pycep_correios/excecoes.py
+++ pycep_correios/excecoes.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 
 
-class CEPInvalido(Exception):
+class ExcecaoBase(Exception):
+    """ Excecao base da lib"""
+
+    def __init__(self, *args, **kwargs):
+        self.msg = kwargs.pop('msg', None)
+        super(ExcecaoBase, self).__init__(*args, **kwargs)
+
+
+class CEPInvalido(ExcecaoBase):
     """ Excecao disparada quando o CEP possui tamanho incorreto
     (diferente de 8) e/ou não existe"""
+
+
+class FalhaNaConexao(ExcecaoBase):
+    """ Erro de conexao durante a requisicao """
+
+
+class MultiploRedirecionamento(ExcecaoBase):
+    """ Excecao de multiplo redirecionamento """
+
+
+class Timeout(ExcecaoBase):
+    """ Erro de TimeOut  """
