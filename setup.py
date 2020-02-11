a65ab109cf48ee20f6641498704cc3ee67d0fb2b
Michell Stuttgart <michellstut@gmail.com>
2020-02-11 19:13:39 -0300

    Add suport to python 3.7

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     tests_require=test_requirements,
