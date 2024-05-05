ff12e68d4a455eb4af7740fc9b848eee050dbc96
Michell Stuttgart <michellstut@gmail.com>
2024-05-05 19:44:54 -0300

    Update test sessions in Nox

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,49 @@
 import nox
 
-@nox.session(python=['3.8', '3.9', '3.10', '3.11'], reuse_venv=True)
-def test(session):
-    session.run('poetry', 'shell', external=True)
-    session.run('poetry', 'install', external=True)
-    session.run('pytest')
+nox.options.sessions = [ "tests", "pylint"]
+
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    """
+    Run the unit and regular tests.
+    """
+    session.install("-e.[test]")
+    session.run("pytest")
+
+
+@nox.session
+def pylint(session: nox.Session) -> None:
+    """
+    Run pylint.
+    """
+    session.install("-e.[lint]")
+    session.run("pylint", "brazilcep")
+
+@nox.session
+def coverage(session: nox.Session) -> None:
+    """
+    Run coverage.
+    """
+    session.install("-e.[coverage]")
+    session.run(
+        "pytest", "--cov=brazilcep", "--cov-report=html", "tests"
+    )
+
+@nox.session
+def check_manifest(session: nox.Session) -> None:
+    """
+    Ensure all needed files are included in the manifest.
+    """
+
+    session.install("check-manifest")
+    session.run("check-manifest", *session.posargs)
+
+@nox.session
+def docs(session: nox.Session) -> None:
+    """
+    Build the docs. Will serve unless --non-interactive
+    """
+    session.install("-e.[docs]")
+    session.run("mkdocs", "serve" if session.interactive else "build")
+
